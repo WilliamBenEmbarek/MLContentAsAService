41fd84e65416f3d25150cb77311d13a4540a14e6
William Ben Embarek <william.benembarek@gmail.com>
2020-06-11 11:23:14 +0200

    Adding artwork

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var ThisPersonDoesNotExist = require('./this-person-does-not-exist')
 var ThisCatDoesNotExist = require('./this-cat-does-not-exist');
+var ThisArtworkDoesNotExist = require('./this-artwork-does-not-exist');
 var express = require("express");
 var app = express();
 
 const personGen = new ThisPersonDoesNotExist();
 const catGen = new ThisCatDoesNotExist();
+const artGen = new ThisArtworkDoesNotExist();
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
@@ -103,6 +105,62 @@ app.get("/cat/:resX/:resY", (req, res, next) => {
         console.log('genertated:', resA);
         var options = {
             root: ('cats'),
+            dotfiles: 'deny',
+            headers: {
+              'x-timestamp': Date.now(),
+              'x-sent': true
+            }
+          }
+        res.sendFile('latest.jpeg', options, function (err) {
+            if (err) {
+                next(err)
+            } else {
+                console.log('Sent image');
+            }
+        })
+    }).catch(err => {
+        console.log('error->', err);
+        next(err);
+    });
+});
+
+app.get("/art", (req, res, next) => {
+    artGen.getImage({
+        width: parseInt(256),
+        height: parseInt(256),
+        path: 'artwork'
+    }).then(resA => {
+        console.log('genertated:', resA);
+        var options = {
+            root: ('artwork'),
+            dotfiles: 'deny',
+            headers: {
+              'x-timestamp': Date.now(),
+              'x-sent': true
+            }
+          }
+        res.sendFile('latest.jpeg', options, function (err) {
+            if (err) {
+                next(err)
+            } else {
+                console.log('Sent image');
+            }
+        })
+    }).catch(err => {
+        console.log('error->', err);
+        next(err);
+    });
+})
+
+app.get("/art/:resX/:resY", (req, res, next) => {
+    artGen.getImage({
+        width: parseInt(req.params.resX),
+        height: parseInt(req.params.resY),
+        path: 'artwork'
+    }).then(resA => {
+        console.log('genertated:', resA);
+        var options = {
+            root: ('artwork'),
             dotfiles: 'deny',
             headers: {
               'x-timestamp': Date.now(),
